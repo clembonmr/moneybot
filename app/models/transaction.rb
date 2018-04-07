4f551dcf5ecc94ff9c337ebe0963cf371466fca9
clement bonnemaire <clement.bonnemaire@gmail.com>
2018-04-07 17:22:41 +0800

    optional references between Transaction and tag category

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,6 +1,6 @@
 class Transaction < ApplicationRecord
   belongs_to :account
-  belongs_to :category
-  belongs_to :tag
+  belongs_to :category, optional: true
+  belongs_to :tag, optional: true
   belongs_to :user, through: :account
 end
