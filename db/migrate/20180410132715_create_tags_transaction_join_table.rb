70764b971b0beffca030f4c8939c004ccfe7c90f
clement bonnemaire <clement.bonnemaire@gmail.com>
2018-04-10 21:34:33 +0800

    corrected tag transaction join table

--- db/migrate/20180410132715_create_tags_transaction_join_table.rb
+++ db/migrate/20180410132715_create_tags_transaction_join_table.rb
@@ -1,7 +1,7 @@
 class CreateTagsTransactionJoinTable < ActiveRecord::Migration[5.1]
   def change
-    create_join_table :categories, :tags do |t|
-      t.index :category_id
+    create_join_table :transactions, :tags do |t|
+      t.index :transaction_id
       t.index :tag_id
     end
   end
